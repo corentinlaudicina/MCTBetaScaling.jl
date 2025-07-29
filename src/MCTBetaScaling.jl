8237fa097913b17d70ed0a60a5090cb447a7a66a
corentin_laudicina <39194817+corentinlaudicina@users.noreply.github.com>
2025-07-29 13:18:48 +0200

    Update MCTBetaScaling.jl

--- src/MCTBetaScaling.jl
+++ src/MCTBetaScaling.jl
@@ -5,8 +5,9 @@ Package to solve mode-coupling theory like equations
 module MCTBetaScaling
     using ModeCouplingTheory, SpecialFunctions
     export BetaScalingEquation
+    export StochasticBetaScalingEquation
 
-    for file in ["HelperFunctions.jl", "BetaScaling.jl"]
+    for file in ["HelperFunctions.jl", "BetaScaling.jl", "StochasticBetaScaling.jl"]
         include(file)
     end
     
