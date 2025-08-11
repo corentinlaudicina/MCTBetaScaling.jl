29a4768981a8003d8cd6d02117bad41e2f944ade
Ilian Pihlajamaa <ilian.pihlajamaa@gmail.com>
2025-08-11 13:55:16 +0200

    add lambda

--- src/MCTBetaScaling.jl
+++ src/MCTBetaScaling.jl
@@ -4,9 +4,10 @@ Package to solve mode-coupling theory like equations
 """
 module MCTBetaScaling
     using ModeCouplingTheory, SpecialFunctions
+    using LinearAlgebra
     export BetaScalingEquation
 
-    for file in ["HelperFunctions.jl", "BetaScaling.jl"]
+    for file in ["HelperFunctions.jl", "BetaScaling.jl", "lambda.jl"]
         include(file)
     end
     
